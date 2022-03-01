3000c87c224748b525092d53be7322ec4e88a5fb
Scott Freitag <scott.d.freitag@gmail.com>
2022-02-28 20:24:30 -0800

    Week 1 Assignment

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
-import { Text, ScrollView } from "react-native";
+import { Text, ScrollView, FlatList } from "react-native";
+import { Card, ListItem } from "react-native-elements";
+import { PARTNERS } from "../shared/partners";
+
+function RenderMission({ item }) {
+  if (item) {
+    return (
+      <Card title="Our Mission">
+        <Text style={{ margin: 10 }}>
+          {
+            "We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other."
+          }
+        </Text>
+      </Card>
+    );
+  }
+  return <View />;
+}
 
 class About extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      partners: PARTNERS,
+    };
   }
 
   static navigationOptions = {
@@ -11,9 +31,26 @@ class About extends Component {
   };
 
   render() {
+    const renderPartner = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: require("./images/bootstrap-logo.png") }}
+        />
+      );
+    };
+
     return (
       <ScrollView>
-        <Text></Text>
+        <RenderMission item={"Our Mission"} />
+        <Card title="Community Partners">
+          <FlatList
+            data={this.state.partners}
+            renderItem={renderPartner}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </Card>
       </ScrollView>
     );
   }
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Card, Text, ScrollView } from "react-native";
+import { Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
 
 class Contact extends Component {
   constructor(props) {
@@ -18,7 +19,11 @@ class Contact extends Component {
           wrapperStyle={{ margin: 20 }}
           style={{ flex: 1 }}
         >
-          <Text></Text>
+          <Text>1 Nucamp Way</Text>
+          <Text>Seattle, WA 98001</Text>
+          <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+          <Text>Phone: 1-206-555-1234</Text>
+          <Text>Email: campsites@nucamp.co</Text>
         </Card>
       </ScrollView>
     );
